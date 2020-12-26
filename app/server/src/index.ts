358c1d27c90cfbf6904e65ebb7748d8af54753e5
Guy <guy7145@gmail.com>
2020-12-26 12:04:27 +0200

    lot's of setup

--- app/server/src/index.ts
+++ app/server/src/index.ts
@@ -3,7 +3,10 @@ import cors from 'cors';
 import hopsList from './data/hops';
 const app = express();
 
-app.use(cors());
+app.options('*', cors());
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080'],
+}));
 
 app.get('/data/all-hops', function (req, res) {
     console.log(`sending data... (${Object.keys(hopsList).length} different hops)`);
@@ -14,8 +17,8 @@ app.get('/data/hop/:hopName', function (req, res) {
     res.json(hopsList[req.params.hopName]);
 });
 
-app.use(express.static('../client/dist/'))
+app.use(express.static('./client-dist'))
 
-const port = 5000;
+const port = 8080;
 console.log(`listening on port ${port}`);
 app.listen(port);
